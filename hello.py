41e39caebbd1670fbc7b4bc6b22ce4b74f6ba65f
Chris Allen <ashper99@gmail.com>
2023-07-06 20:47:53 -0700

    Learned forms and how to use submit

--- hello.py
+++ hello.py
@@ -1,8 +1,53 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask Instance
 app = Flask(__name__) # way to always start.  finds files
+app.config['SECRET_KEY'] = "my super secret key"
+
+# Create a form class
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name?", validators =[DataRequired()])
+	submit = SubmitField("Submit")
+
+# BooleanField
+# DateField
+# DateTimeField
+# DecimalField
+# FileField
+# HiddenField
+# MultipleField
+# FieldList
+# FloatField
+# FormField
+# IntegerField
+# PasswordField
+# RadioField
+# SelectField
+# SelectMultipleField
+# SubmitField
+# StringField
+# TextAreaField
+
+## Validators
+# DataRequired
+# Email
+# EqualTo
+# InputRequired
+# IPAddress
+# Length
+# MacAddress
+# NumberRange
+# Optional
+# Regexp
+# URL
+# UUID
+# AnyOf
+# NoneOf
+
 
 # Create a route decorator
 @app.route('/') # main webpage
@@ -49,5 +94,22 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+#Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+
+	#Validate Form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+
+
+	return render_template("name.html", name = name,
+		form = form)
+
+
 
 
